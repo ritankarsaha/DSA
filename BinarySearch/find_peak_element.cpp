e30b7ffa8276eb213483c49bfe5af62210416121
Ritankar Saha <ritankar.saha786@gmail.com>
2024-04-21 06:02:36 +0530

    dsa

--- BinarySearch/find_peak_element.cpp
+++ BinarySearch/find_peak_element.cpp
@@ -18,7 +18,7 @@ public:
             int mid = (low + high) / 2;
             if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1])
                 return mid;
-            // left portion elimination
+            // left portion elimination.
             if (nums[mid] > nums[mid - 1])
                 low = mid + 1;
             else
