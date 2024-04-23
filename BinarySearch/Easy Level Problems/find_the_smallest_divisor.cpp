2443b6fd918cfdbce710924cc7507721e61cc189
Ritankar Saha <ritankar.saha786@gmail.com>
2024-04-24 02:26:55 +0530

    modified:   BinarySearch/Easy Level Problems/find_the_smallest_divisor.cpp

--- BinarySearch/Easy Level Problems/find_the_smallest_divisor.cpp
+++ BinarySearch/Easy Level Problems/find_the_smallest_divisor.cpp
@@ -50,7 +50,7 @@ Explanation: We can get a sum to 17 (1+2+5+9) if the divisor is 1.
 If the divisor is 4 we can get a sum of 7 (1+1+2+3) and if the divisor is 5 the sum will be 5 (1+1+1+2).
 Example 2:
 
-Input: nums = [44,22,33,11,1], threshold = 5
+Input: nums = [44,22,  33,11,1], threshold = 5
 Output: 44
 
 
