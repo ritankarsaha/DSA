a540eebc2e3b1ef43307222e8a913f02a090b7fa
Ritankar Saha <ritankar.saha786@gmail.com>
2024-05-12 09:20:53 +0530

    modified:   DSA_Questions/Arrays/Easy Problems/Second_Largest_Element_In_An_Array.cpp 	new file:   DSA_Questions/Arrays/Easy Problems/Second_Smallest_Element_in_An_Array.cpp

--- DSA_Questions/Arrays/Easy Problems/Second_Largest_Element_In_An_Array.cpp
+++ DSA_Questions/Arrays/Easy Problems/Second_Largest_Element_In_An_Array.cpp
@@ -11,7 +11,18 @@ using namespace std;
 
 
 int SecondLargest(vector<int>v){
-    
+    int largest = INT_MIN,slargest=INT_MIN;
+    for(int i=0;i<v.size();i++){
+        if(v[i]>largest){
+            slargest = largest;
+            largest = v[i];
+        }
+        else if(v[i] > largest &&v[i]!=largest)
+        {
+            slargest = v[i];
+        }
+    }
+    return slargest;
 }
 int main()
 {
