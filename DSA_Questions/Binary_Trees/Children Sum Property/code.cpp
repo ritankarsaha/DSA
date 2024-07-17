eb091a20540d418f11fc5bc9027d625540a3446e
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-18 02:08:05 +0530

    binary tree problems

---
+++ DSA_Questions/Binary_Trees/Children Sum Property/code.cpp
@@ -0,0 +1,32 @@
+class Solution{
+    public:
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+    bool isLeaf(Node* root){
+        if(root->left == NULL && root->right == NULL)
+        return true;
+        
+        return false;
+    }
+    int isSumProperty(Node *root)
+    {
+      if(!root)
+      return 1;
+      if(isLeaf(root))
+      return 1;
+      
+      int left = isSumProperty(root->left);
+      int right = isSumProperty(root->right);
+      if(!left || !right)
+      return 0;
+      int sum = 0;
+      if(root->left) 
+      sum+=root->left->data;
+      if(root->right)
+      sum+=root->right->data;
+      if(sum == root->data)
+      return 1;
+      
+      return 0;
+    }
+};
