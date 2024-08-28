222395274330131c2752cd0c81900a69460e2d2b
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-29 01:22:30 +0530

    graphs questions

--- DSA_Questions/Sliding_Window_and_Two_Pointer_Questions/longest substring without repeating characters/brute.cpp
+++ DSA_Questions/Sliding_Window_and_Two_Pointer_Questions/longest substring without repeating characters/brute.cpp
@@ -11,19 +11,25 @@ using namespace std;
 
 int main()
 {
-    string s;
-    int n = s.length(),len = 0,maxlen=0;
-   for(int i=0;i<n;i++)
-   {
-    int hash[256] = {0};
-    for(int j=i;j<n;j++)
-    {
-        if(hash[s[j]] == 1)
-        break;
-        len = j - i +1;
-        maxlen = max(len,maxlen);
-        hash[s[j]] == 1;
-    }
-   }
-   cout << maxlen;
+
 }
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int len =0,maxlen =0;
+        for(int i=0;i<s.length();i++){
+            vector<int> hash(256,0);
+            for(int j=i;j<s.length();j++){
+                if(hash[s[j]] == 1)
+                break;
+                len = (j-i+1);
+                maxlen = max(len,maxlen);
+                hash[s[j]] = 1;
+
+            }
+        }
+
+        return maxlen;
+        
+    }
+};
