8fc90a4af0fb70536f674d7c3a91f9fcd0a0f6bc
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-09-02 00:57:58 +0530

    Path with min effort

---
+++ DSA_Questions/Graphs/Shortest Path Algorithms/Path_With_Minimum_Effort.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+
+        int n = heights.size();
+        int m = heights[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        dist[0][0] = 0;
+        pq.push({0,{0,0}});
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            if(row == n-1 && col == m-1)
+                return diff;
+            for(int i = 0; i < 4; i++){
+                int newr = row + dr[i];
+                int newc = col + dc[i];
+                if(newr >= 0 && newc >= 0 && newr < n && newc < m){
+                    int effort = max(abs(heights[row][col] - heights[newr][newc]), diff);
+                    if(effort < dist[newr][newc]){
+                        dist[newr][newc] = effort;
+                        pq.push({effort, {newr, newc}});
+                    }
+                }
+            }
+        }
+
+        return 0;  
+    }
+};
