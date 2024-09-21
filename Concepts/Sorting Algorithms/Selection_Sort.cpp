c72346354da1ca71f000a0345d8a5b15fb1c2448
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-09-21 18:58:30 +0530

    Seelction Sort

---
+++ Concepts/Sorting Algorithms/Selection_Sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void selectionsort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        int min_idx = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_idx]){
+                min_idx = j;
+            }
+        }
+        swap(arr[min_idx],arr[i]);
+    }
+}
