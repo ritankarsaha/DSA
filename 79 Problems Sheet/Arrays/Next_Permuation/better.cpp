849825ebcf54ae553c8bf4ee9a1d31fa86daafbc
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-07 02:31:02 +0530

    Next Permutation

---
+++ 79 Problems Sheet/Arrays/Next_Permuation/better.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<math.h>
+#include<utility>
+#include<stack>
+#include<list>
+#include<map>
+using namespace std;
+
+int main()
+{
+
+}
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
