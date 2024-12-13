17ab32077ba43526e71b96725e49f243869bb922
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-13 15:48:23 +0530

    Preorder Traversal

---
+++ DSA_Questions/Binary_Search_Trees/Construct_From_Preorder_Traversla/code.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return Builder(preorder,i,INT_MAX);  
+    }
+
+
+    TreeNode* Builder(vector<int>& preorder, int & i, int bound){
+        if(i == preorder.size() || preorder[i]>bound)
+        return NULL;
+        TreeNode* node = new TreeNode(preorder[i++]);
+        node->left = Builder(preorder,i,node->val);
+        node->right = Builder(preorder,i,bound);
+
+        return node;
+    }
+};
