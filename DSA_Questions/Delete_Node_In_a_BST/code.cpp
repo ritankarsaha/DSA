d17eab2c32858ac24efe4563cbde736cf9156717
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-12-13 20:32:57 +0530

    BST CODES

---
+++ DSA_Questions/Delete_Node_In_a_BST/code.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode helper(TreeNode root){
+        if(root.left == null)
+        return root.right;
+        if(root.right == null)
+        return root.left;
+        TreeNode rightChild = root.right;
+        TreeNode lastright  = findLast(root.left);
+        lastright.right = root.right;
+        return root.left;
+    }
+
+    public TreeNode findLast(TreeNode root){
+        while(root.right != null)
+        root = root.right;
+        return root;
+    }
+
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null)
+        return root;
+        if(root.val == key)
+        return helper(root);
+
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr.val > key){
+                if(curr.left != null && curr.left.val == key){
+                    curr.left = helper(curr.left);
+                }
+                else curr = curr.left;
+            }
+
+            else{
+                if(curr.right != null && curr.right.val == key){
+                    curr.right  = helper(curr.right);
+                }
+                else curr = curr.right;
+            }
+        }
+
+        return root;
+
+
+
+    }
+}
